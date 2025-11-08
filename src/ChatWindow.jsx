fc22be941fb2e1ddc4406259e80011d0e320c626
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2025-11-08 20:49:49 +0530

    changes

--- src/ChatWindow.jsx
+++ src/ChatWindow.jsx
@@ -30,7 +30,7 @@ function ChatWindow() {
         };
 
         try {
-            const response = await fetch("http://localhost:3030/api/chat", options);
+            const response = await fetch("http://chai-backend.vercel.app/api/chat", options);
             const res = await response.json();
             // console.log(res);
             setReply(res.reply);
